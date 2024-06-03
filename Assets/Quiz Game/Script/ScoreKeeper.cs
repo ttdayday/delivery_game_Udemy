e94e921f27abb6ddce4c7c9b4eead69854c1e581
tiantianttd <tidin@microsoft.com>
2024-06-02 21:51:20 -0700

    setter getter

---
+++ Assets/Quiz Game/Script/ScoreKeeper.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    int correctAnwers = 0;
+    int questionsSeen = 0;
+
+
+    public int GetCorrectAnswers()
+    {
+        return GetCorrectAnswers();
+    }
+
+    public void IncrementCorrectAnswers()
+    {
+        correctAnwers++;
+    }
+}
